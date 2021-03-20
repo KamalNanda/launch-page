2fdd3d4598ae0f7fd7ad6ffebb8f4db08c06e29f
Kamal Nanda <kamalnanda45@gmail.com>
2021-03-20 22:34:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,8 @@ db.once('open', function(callback){
 }) 
   
 var app=express() 
-  
-var allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "*");
-
-  next();
-};
-  
-app.use(allowCrossDomain);
+   
+   
 app.use(bodyParser.json()); 
 app.use(express.static('public')); 
 app.use(bodyParser.urlencoded({ 
